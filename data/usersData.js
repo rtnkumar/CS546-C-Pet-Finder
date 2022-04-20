420770dab6dc0d78a4b131a9c71796832451238e
rtnkumar <rkumar16@stevens.edu>
2022-04-20 03:02:24 -0400

    Added user sign-up api

--- data/usersData.js
+++ data/usersData.js
@@ -1,20 +1,126 @@
 const mongoCollections = require('../config/mongoCollections');
+const validators = require('../validators');
+const emailValidator = require("email-validator");
 const users = mongoCollections.users;
 const pets = mongoCollections.pets;
 const petTypes = mongoCollections.petTypes;
 const petsQuestionsAnswers = mongoCollections.petsQuestionsAnswers;
+const commonValidators = validators.commonValidators;
+const utils = require('../utils/utils');
 
 
-// 1. Create Band
-async function create(firstName, middleName, lastName, email, phoneNumber, password, address, city, state, zip, picture) {
+// 1. Create User
+async function createUser(firstName, middleName, lastName, email, phoneNumber, password, address, city, state, zip, picture) {
+    if (arguments.length != 11) {
+        throw 'Only 11 arguments are required';
+    }
+
+    // FirstName string validation
+    let isValidFirstName = commonValidators.isValidString(firstName, 'firstName');
+    if (!isValidFirstName[0]) {
+        throw isValidFirstName[1];
+    }
+
+    // FirstName alphabet validation
+    isValidFirstName = commonValidators.isValidAlphabet(firstName, 'firstName');
+    if (!isValidFirstName[0]) {
+        throw isValidFirstName[1];
+    }
+
+    // MiddleName validation
+    if (middleName) {
+        let isValidMiddleName = commonValidators.isValidString(middleName, 'middleName');
+        if (!isValidMiddleName[0]) {
+            throw isValidMiddleName[1];
+        }
+
+        isValidMiddleName = commonValidators.isValidAlphabet(middleName, 'middleName');
+        if (!isValidMiddleName[0]) {
+            throw isValidMiddleName[1];
+        }
+    }
+
+    // LastName string validation
+    let isValidLastName = commonValidators.isValidString(lastName, 'lastName');
+    if (!isValidLastName[0]) {
+        throw isValidLastName[1];
+    }
+
+    // LastName alphabet validation
+    isValidLastName = commonValidators.isValidAlphabet(lastName, 'lastName');
+    if (!isValidLastName[0]) {
+        throw isValidLastName[1];
+    }
+
+    // Email validation
+    if (!email) {
+        throw `email is required`;
+    }
+    if (!emailValidator.validate(email)) {
+        throw `${email} is invalid email`;
+    }
+
+    // PhoneNumber validation
+    if (!phoneNumber) {
+        throw 'phoneNumber is required';
+    }
+    let isValidPhoneNumber = commonValidators.isValidPhoneNumber(phoneNumber, 'phoneNumber');
+    if (!isValidPhoneNumber[0]) {
+        throw isValidPhoneNumber[1];
+    }
+
+    // Password validation
+    let isValidPassword = commonValidators.isValidString(password, 'password');
+    if (!isValidPassword[0]) {
+        throw isValidPassword[1];
+    }
+
+    if (password.length < 6) {
+        throw 'password should have at least 6 characters'
+    }
+
+    // Address string validation
+    let isValidAddress = commonValidators.isValidString(address, 'address');
+    if (!isValidAddress[0]) {
+        throw isValidAddress[1];
+    }
+
+    // City string validation
+    let isValidCity = commonValidators.isValidString(city, 'city');
+    if (!isValidCity[0]) {
+        throw isValidCity[1];
+    }
+
+    // State string validation
+    let isValidState = commonValidators.isValidString(state, 'state');
+    if (!isValidState[0]) {
+        throw isValidState[1];
+    }
+
+    // Zip string validation
+    let isValidZip = commonValidators.isValidString(zip, 'zip');
+    if (!isValidZip[0]) {
+        throw isValidZip[1];
+    }
+
+    isValidZip = commonValidators.isValidInteger(zip, 'zip');
+    if (!isValidZip[0]) {
+        throw isValidZip[1];
+    }
+
+    email=email.trim();
+    let user=await getUserByEmail(email);
+    if(user){
+        throw `This ${email} is already exist, please use another`
+    }
 
     let newUser = {
         firstName: firstName.trim(),
         middleName: middleName.trim(),
         lastName: lastName.trim(),
-        email: email.trim(),
+        email: email,
         phoneNumber: phoneNumber.trim(),
-        password: password.trim(),
+        password: await utils.getHashedPassword(password.trim()),
         address: address.trim(),
         city: city.trim(),
         state: state.trim(),
@@ -28,10 +134,22 @@ async function create(firstName, middleName, lastName, email, phoneNumber, passw
     if (!insertInfo.acknowledged || !insertInfo.insertedId)
         throw 'Could not add band';
 
-    const newId = insertInfo.insertedId.toString();
-    return newId;
+    return await getUserByEmail(email);
+}
+
+async function getUserByEmail(email) {
+    // Email validation
+    if (!email) {
+        throw `email is required`;
+    }
+    if (!emailValidator.validate(email)) {
+        throw `${email} is invalid email`;
+    }
+
+    const usersCollection = await users();
+    return await usersCollection.findOne({ email: email });
 }
 
 module.exports = {
-    create
+    createUser
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,33 +1,291 @@
 const express = require('express');
 const usersRouter = express.Router();
 const xss = require('xss');
+const path = require('path');
+const formidable = require('formidable');
+const validators = require('../validators');
+const commonValidators = validators.commonValidators;
+const emailValidator = require("email-validator");
+
 
 
 const data = require('../data');
 const usersData = data.usersData;
 
-usersRouter
-    .route('/sign-up')
-    .post(async (request, res) => {
-        const firstName = xss(request.body.firstName);
-        const middleName = xss(request.body.middleName);
-        const lastName = xss(request.body.LastName);
-        const email = xss(request.body.email);
-        const phoneNumber = xss(request.body.phoneNumber);
-        const password = xss(request.body.password);
-        const address = xss(request.body.address);
-        const city = xss(request.body.city);
-        const state = xss(request.body.state);
-        const zip = xss(request.body.zip);
-        const picture = xss(request.body.picture);
 
-        try {
-            const result = await usersData.create(firstName, middleName, lastName, email, phoneNumber, password, address, city, state, zip, picture);
-            res.json({ id: result });
-        } catch (e) {
-            res.json({ error: e });
+usersRouter
+  .post('/sign-up', async (request, res) => {
+    try {
+      let form = new formidable.IncomingForm();
+      form.parse(request, async (err, fields, files) => {
+        if (err) {
+          return res.status(400).json({
+            status: "Fail",
+            message: "There was an error parsing the files",
+            error: err,
+          });
+        }
+        else if (!files || Object.keys(files).length == 0 || !files.picture) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            picture: "picture is required"
+          });
+        }
+        else if (files.picture.size <= 0) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            picture: "picture size is zero"
+          });
+        }
+        const isValid = commonValidators.isValidFile(files.picture);
+        if (!isValid) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            picture: "Only jpg, jpeg or png are required!"
+          });
         }
 
+        const firstName = xss(fields.firstName);
+        const middleName = xss(fields.middleName);
+        const lastName = xss(fields.lastName);
+        const email = xss(fields.email);
+        const phoneNumber = xss(fields.phoneNumber);
+        const password = xss(fields.password);
+        const address = xss(fields.address);
+        const city = xss(fields.city);
+        const state = xss(fields.state);
+        const zip = xss(fields.zip);
+        const picture = xss(files.picture.originalFilename);
+
+        // FirstName string validation
+        let isValidFirstName = commonValidators.isValidString(firstName, 'firstName');
+        if (!isValidFirstName[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            firstName: isValidFirstName[1]
+          });
+        }
+
+        // FirstName alphabet validation
+        isValidFirstName = commonValidators.isValidAlphabet(firstName, 'firstName');
+        if (!isValidFirstName[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            firstName: isValidFirstName[1]
+          });
+        }
+
+        // MiddleName validation
+        if (middleName) {
+          let isValidMiddleName = commonValidators.isValidString(middleName, 'middleName');
+          if (!isValidMiddleName[0]) {
+            return res.status(400).json({
+              error: true,
+              message: "Invalid input",
+              middleName: isValidMiddleName[1]
+            });
+          }
+
+          isValidMiddleName = commonValidators.isValidAlphabet(middleName, 'middleName');
+          if (!isValidMiddleName[0]) {
+            return res.status(400).json({
+              error: true,
+              message: "Invalid input",
+              middleName: isValidMiddleName[1]
+            });
+          }
+        }
+
+        // LastName string validation
+        let isValidLastName = commonValidators.isValidString(lastName, 'lastName');
+        if (!isValidLastName[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            lastName: isValidLastName[1]
+          });
+        }
+
+        // LastName alphabet validation
+        isValidLastName = commonValidators.isValidAlphabet(lastName, 'lastName');
+        if (!isValidLastName[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            lastName: isValidLastName[1]
+          });
+        }
+
+        // Email validation
+        if (!email) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            email: `email is required`
+          })
+        }
+
+        if (!emailValidator.validate(email)) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            email: `${email} is invalid email`
+          })
+        }
+
+        // PhoneNumber validation
+        if (!phoneNumber) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            phoneNumber: "phoneNumber is required"
+          })
+        }
+
+        let isValidPhoneNumber = commonValidators.isValidPhoneNumber(phoneNumber, 'phoneNumber');
+        if (!isValidPhoneNumber[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            phoneNumber: isValidPhoneNumber[1]
+          })
+        }
+
+        // Password validation
+        let isValidPassword = commonValidators.isValidString(password, 'password');
+        if (!isValidPassword[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            password: isValidPassword[1]
+          });
+        }
+
+        if (password.length < 6) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            password: 'password should have at least 6 characters'
+          });
+        }
+
+        // Address string validation
+        let isValidAddress = commonValidators.isValidString(address, 'address');
+        if (!isValidAddress[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            address: isValidAddress[1]
+          });
+        }
+
+        // City string validation
+        let isValidCity = commonValidators.isValidString(city, 'city');
+        if (!isValidCity[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            city: isValidCity[1]
+          });
+        }
+
+        // State string validation
+        let isValidState = commonValidators.isValidString(state, 'state');
+        if (!isValidState[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            state: isValidState[1]
+          });
+        }
+
+        // Zip string validation
+        let isValidZip = commonValidators.isValidString(zip, 'zip');
+        if (!isValidZip[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            zip: isValidZip[1]
+          });
+        }
+
+        isValidZip = commonValidators.isValidInteger(zip, 'zip');
+        if (!isValidZip[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            zip: isValidZip[1]
+          });
+        }
+
+        try {
+          const result = await usersData.createUser(firstName, middleName, lastName, email, phoneNumber, password, address, city, state, zip, picture);
+         // return res.json({ id: result, firstName: firstName, middleName: middleName, lastName: lastName, email: email, phoneNumber: phoneNumber, password: password, address: address, city: city, state: state, zip: picture });
+          return res.json(result);
+        
+        } catch (e) {
+          if (e === `This ${email.trim()} is already exist, please use another`) {
+            return res.status(400).json({
+              error: true,
+              message: "Invalid input",
+              email: `This ${email.trim()} is already exist, please use another`
+            });
+          } else {
+            return res.status(500).json({
+              error: true,
+              message: e,
+            });
+          }
+        }
+
+      });
+
+      form.on('fileBegin', function (name, file) {
+        file.filepath = 'public/uploads/images/users/' + file.originalFilename;
+      });
+
+    } catch (e) {
+      return res.status(500).json({
+        error: true,
+        message: e,
+      });
+    }
+  });
+
+usersRouter.
+  post('/upload', async (req, res) => {
+    var form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      console.log(fields);
+      field = fields;
+      console.log(files.upload.size);
+      const isValid = isFileValid(files.upload);
+      console.log("isValid+" + isValid);
+      if (err) {
+        console.log("Error parsing the files");
+        return res.status(400).json({
+          status: "Fail",
+          message: "There was an error parsing the files",
+          error: err,
+        });
+      }
     });
+
+    form.on('fileBegin', function (name, file) {
+      file.filepath = 'public/uploads/images/users/' + file.originalFilename;
+    });
+
+    // form.on('file', function (name, file){
+    //     console.log('Uploaded ' + file.originalFilename);
+    // });
+
+    res.sendFile(path.resolve('static/home.html'));
+
+  });
 
 module.exports = usersRouter;
