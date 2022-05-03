a9ab547f18db610341482369a012bc01bf571fbf
Feneel Doshi <feneel@Feneels-MacBook-Air.local>
2022-05-03 00:49:55 -0400

    Added getUserDetails, removeUser and updateUserDetails  API

--- data/usersData.js
+++ data/usersData.js
@@ -356,9 +356,271 @@ async function getUserById(id){
     return user;
 }
 
+/** 
+*Feneel Doshi
+*Retrieve the email id of user
+* @param {email of user} emailId
+* @returns
+*/
+
+async function getUserDetailsByEmail(emailId) {
+
+    //Input Arguments validation
+    if (arguments.length != 1)
+        throw "Error: There should not be more than 1 arguments!"
+
+
+    //Email validation
+    if (!emailId || emailId.trim() == "") {
+        throw "Error: Email cannot be empty"
+    }
+
+    if (!emailValidator.validate(emailId)) {
+        throw `${emailId} is not a valid email!`
+    }
+
+
+    emailId = emailId.trim();
+    emailId = emailId.toLowerCase();
+
+    const usersCollection = await users();
+    const user = await usersCollection.findOne({ email: emailId });
+    if (user === null) {
+        throw "Email doesn't exist";
+    }
+
+    // favorite pet list
+    let favoriteIdList = user.favoriteList;
+    let favoriteListObjectIdList = [];
+    for (id of favoriteIdList) {
+        favoriteListObjectIdList.push(ObjectId(id));
+    }
+    const petCollection = await pets();
+    const favoritePetsList = await petCollection.find({ _id: { $in: favoriteListObjectIdList } }).toArray();
+    for (let pet of favoritePetsList) {
+        pet._id = pet._id.toString();
+    }
+
+    // adopted pet list
+    let adoptedIdList = user.adoptedList;
+    let adoptedListObjectIdList = [];
+    for (id of adoptedIdList) {
+        adoptedListObjectIdList.push(ObjectId(id));
+    }
+    const adoptedPetsList = await petCollection.find({ _id: { $in: adoptedListObjectIdList } }).toArray();
+    for (let pet of adoptedPetsList) {
+        pet._id = pet._id.toString();
+    }
+
+    user.favoriteList = favoritePetsList;
+    user.adoptedList = adoptedPetsList;
+    delete user.password;
+    return user;
+
+}
+
+
+/**
+ * Feneel Doshi
+ * Update user
+ * 
+ * @param {First Name of user} firstName
+ * @param {Middle Name of user} middleName
+ * @param {Last Name of user} lastName
+ * @param {Phone Number of user} phoneNumber
+ * @param {Address of user} address
+ * @param {City of user} city
+ * @param {State of user} state
+ * @param {Zip of user} zip 
+ * @returns
+*/
+
+async function updateUserProfile(firstName, middleName, lastName, email, phoneNumber, address, city, state, zip, picture) {
+    // FirstName string validation
+    let isValidFirstName = commonValidators.isValidString(firstName, 'firstName');
+    if (!isValidFirstName[0]) {
+        throw isValidFirstName[1];
+    }
+
+    // FirstName alphabet validation
+    isValidFirstName = commonValidators.isValidName(firstName, 'firstName');
+    if (!isValidFirstName[0]) {
+        throw isValidFirstName[1];
+    }
+
+    // MiddleName validation
+    if (middleName) {
+        let isValidMiddleName = commonValidators.isValidString(middleName, 'middleName');
+        if (!isValidMiddleName[0]) {
+            throw isValidMiddleName[1];
+        }
+
+        isValidMiddleName = commonValidators.isValidName(middleName, 'middleName');
+        if (!isValidMiddleName[0]) {
+            throw isValidMiddleName[1];
+        }
+    }
+
+    // LastName string validation
+    let isValidLastName = commonValidators.isValidString(lastName, 'lastName');
+    if (!isValidLastName[0]) {
+        throw isValidLastName[1];
+    }
+
+    // LastName alphabet validation
+    isValidLastName = commonValidators.isValidName(lastName, 'lastName');
+    if (!isValidLastName[0]) {
+        throw isValidLastName[1];
+    }
+
+    // Email validation
+    if (!email || email.trim() == "") {
+        throw `email is required`;
+    }
+    if (!emailValidator.validate(email)) {
+        throw `${email} is invalid email format`;
+    }
+
+    // PhoneNumber validation
+    if (!phoneNumber) {
+        throw 'phoneNumber is required';
+    }
+    let isValidPhoneNumber = commonValidators.isValidPhoneNumber(phoneNumber, 'phoneNumber');
+    if (!isValidPhoneNumber[0]) {
+        throw isValidPhoneNumber[1];
+    }
+
+    // Address string validation
+    let isValidAddress = commonValidators.isValidString(address, 'address');
+    if (!isValidAddress[0]) {
+        throw isValidAddress[1];
+    }
+
+    isValidAddress = commonValidators.isValidAddress(address, 'address');
+    if (!isValidAddress[0]) {
+        throw isValidAddress[1];
+    }
+
+    // City string validation
+    let isValidCity = commonValidators.isValidString(city, 'city');
+    if (!isValidCity[0]) {
+        throw isValidCity[1];
+    }
+
+    isValidCity = commonValidators.isValidName(city, 'city');
+    if (!isValidCity[0]) {
+        throw isValidCity[1];
+    }
+
+
+    // State string validation
+    let isValidState = commonValidators.isValidString(state, 'state');
+    if (!isValidState[0]) {
+        throw isValidState[1];
+    }
+
+    isValidState = commonValidators.isValidName(state, 'state');
+    if (!isValidState[0]) {
+        throw isValidState[1];
+    }
+
+    // Zip string validation
+    let isValidZip = commonValidators.isValidString(zip, 'zip');
+    if (!isValidZip[0]) {
+        throw isValidZip[1];
+    }
+
+    isValidZip = commonValidators.isValidInteger(zip, 'zip');
+    if (!isValidZip[0]) {
+        throw isValidZip[1];
+    }
+
+
+    email = email.trim();
+    email = email.toLowerCase();
+
+    let user = await getUserByEmail(email);
+    if (!user) {
+        throw `user doesn't exist`
+    }
+
+    let updateUserInfo = {
+        firstName: firstName.trim(),
+        middleName: middleName.trim(),
+        lastName: lastName.trim(),
+        phoneNumber: phoneNumber.trim(),
+        address: address.trim(),
+        city: city.trim(),
+        state: state.trim(),
+        zip: zip.trim(),
+        picture: picture.trim()
+    };
+
+    const usersCollection = await users();
+
+    const updatedInfo = await usersCollection.updateOne(
+        { email: email },
+        { $set: updateUserInfo }
+    )
+
+    if (updatedInfo.modifiedCount == 0) {
+        throw "No update made to the user profile"
+    }
+
+    let updatedUserInfo = await getUserByEmail(email)
+    delete updatedUserInfo.email;
+    delete updatedUserInfo.password;
+    return updatedUserInfo;
+}
+
+
+/**
+ * Feneel Doshi
+ * Remove account of the user
+ * @param {Email of the user} emailId 
+ * @returns 
+ */
+
+async function remove(emailId) {
+
+    if (arguments.length != 1) {
+        throw 'Only 1 argument are required';
+    }
+
+    // Email validation
+    if (!emailId || emailId.trim() == "") {
+        throw `email is required`;
+    }
+
+    if (!emailValidator.validate(emailId)) {
+        throw `${emailId} is invalid email format`;
+    }
+
+    emailId = emailId.trim();
+    emailId = emailId.toLowerCase();
+    const usersCollection = await users()
+    const user = await usersCollection.findOne({ email: emailId })
+
+    if (user === null) {
+        throw `No user with email=${emailId}`;
+    }
+
+
+    const deletionInfo = await usersCollection.deleteOne({ email: emailId })
+
+    if (deletionInfo.deletedCount === 0) {
+        throw `Could not delete user with email of ${emailId}`;
+    }
+
+    return { deleted: true }
+}
+
 module.exports = {
     createUser,
     checkUser,
     updateUserEmailPassword,
-    getUserById
+    getUserById,
+    getUserDetailsByEmail,
+    updateUserProfile,
+    remove
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -565,4 +565,328 @@ usersRouter.
     }
   });
 
+  
+/**
+ * Route for getting user details
+ * Feneel Doshi
+*/
+usersRouter.get("/user-details",trimRequest.all,async (req, res) => {
+  
+  const email = xss(req.body.email);
+  try {
+    // Email validation
+    if (!email || email.trim() == "") {
+      return res.status(400).json({
+        error: true,
+        message: "Invalid input",
+        email: `email is required`
+      })
+    }
+
+    if (!emailValidator.validate(email)) {
+      return res.status(400).json({
+        error: true,
+        message: "Invalid input",
+        email: `${email} is invalid email format`
+      })
+    }
+    const userDetails = await usersData.getUserDetailsByEmail(email);
+    res.json(userDetails);
+  } catch (error) {
+    if (error === `Email doesn't exist`) {
+      return res.status(404).json({
+        error: true,
+        message: "Email doesn't exist",
+      });
+    } else {
+      return res.status(500).json({
+        error: true,
+        message: "Something went wrong, please try after sometime",
+      });
+    }
+  }
+});
+usersRouter.post("/profile/update",async (request, res) => {
+
+  try {
+    let form = new formidable.IncomingForm();
+    form.parse(request, async (err, fields, files) => {
+      if (err) {
+        return res.status(400).json({
+          error: true,
+          message: "There was an error parsing the files",
+        });
+      }
+      else if (!files || Object.keys(files).length == 0 || !files.picture) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          picture: "picture is required"
+        });
+      }
+      else if (files.picture.size <= 0) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          picture: "picture size is zero"
+        });
+      }
+      const isValid = commonValidators.isValidFile(files.picture);
+      if (!isValid) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          picture: "Only jpg, jpeg or png are required!"
+        });
+      }
+      // Schema validation
+      const requestKeyList = Object.keys(fields);
+      const postBodyKeys = ["firstName", "middleName", "lastName", "phoneNumber", "address", "city", "state", "zip"];
+
+      for (let requestKey of postBodyKeys) {
+        if (requestKeyList.indexOf(requestKey) === -1) {
+          return res.status(400).json({
+            error: true,
+            message: `${requestKey} key is missing in body`,
+          });
+        }
+      }
+      if (requestKeyList.length !== postBodyKeys.length) {
+        return res.status(400).json({
+          error: true,
+          message: "Json body is invalid",
+        });
+      }
+      Object.keys(fields).forEach(function (key) {
+        fields[key] = (fields[key]).trim();
+      });
+      const firstName = xss(fields.firstName);
+      const middleName = xss(fields.middleName);
+      const lastName = xss(fields.lastName);
+      const phoneNumber = xss(fields.phoneNumber);
+      const address = xss(fields.address);
+      const city = xss(fields.city);
+      const state = xss(fields.state);
+      const zip = xss(fields.zip);
+      const picture = xss(files.picture.originalFilename);
+
+      // FirstName string validation
+      let isValidFirstName = commonValidators.isValidString(firstName, 'firstName');
+      if (!isValidFirstName[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          firstName: isValidFirstName[1]
+        });
+      }
+
+      // FirstName alphabet validation
+      isValidFirstName = commonValidators.isValidName(firstName, 'firstName');
+      if (!isValidFirstName[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          firstName: isValidFirstName[1]
+        });
+      }
+
+      // MiddleName validation
+      if (middleName) {
+        let isValidMiddleName = commonValidators.isValidString(middleName, 'middleName');
+        if (!isValidMiddleName[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            middleName: isValidMiddleName[1]
+          });
+        }
+
+        isValidMiddleName = commonValidators.isValidName(middleName, 'middleName');
+        if (!isValidMiddleName[0]) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            middleName: isValidMiddleName[1]
+          });
+        }
+      }
+
+      // LastName string validation
+      let isValidLastName = commonValidators.isValidString(lastName, 'lastName');
+      if (!isValidLastName[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          lastName: isValidLastName[1]
+        });
+      }
+
+      // LastName alphabet validation
+      isValidLastName = commonValidators.isValidName(lastName, 'lastName');
+      if (!isValidLastName[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          lastName: isValidLastName[1]
+        });
+      }
+
+      // PhoneNumber validation
+      if (!phoneNumber) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          phoneNumber: "phoneNumber is required"
+        })
+      }
+
+      let isValidPhoneNumber = commonValidators.isValidPhoneNumber(phoneNumber, 'phoneNumber');
+      if (!isValidPhoneNumber[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          phoneNumber: isValidPhoneNumber[1]
+        })
+      }
+
+      // Address string validation
+      let isValidAddress = commonValidators.isValidString(address, 'address');
+      if (!isValidAddress[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          address: isValidAddress[1]
+        });
+      }
+
+      isValidAddress = commonValidators.isValidAddress(address, 'address');
+      if (!isValidAddress[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          address: isValidAddress[1]
+        });
+      }
+
+      // City string validation
+      let isValidCity = commonValidators.isValidString(city, 'city');
+      if (!isValidCity[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          city: isValidCity[1]
+        });
+      }
+
+      isValidCity = commonValidators.isValidName(city, 'city');
+      if (!isValidCity[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          city: isValidCity[1]
+        });
+      }
+
+      // State string validation
+      let isValidState = commonValidators.isValidString(state, 'state');
+      if (!isValidState[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          state: isValidState[1]
+        });
+      }
+
+      isValidState = commonValidators.isValidName(state, 'state');
+      if (!isValidState[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          state: isValidState[1]
+        });
+      }
+
+      // Zip string validation
+      let isValidZip = commonValidators.isValidString(zip, 'zip');
+      if (!isValidZip[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          zip: isValidZip[1]
+        });
+      }
+
+      isValidZip = commonValidators.isValidInteger(zip, 'zip');
+      if (!isValidZip[0]) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid input",
+          zip: isValidZip[1]
+        });
+      }
+
+      try {
+        const result = await usersData.updateUserProfile(firstName, middleName, lastName, request.session.email, phoneNumber, address, city, state, zip, picture);
+        return res.json(result);
+      } catch (e) {
+        if (e === `user doesn't exist`) {
+          return res.status(400).json({
+            error: true,
+            message: "Invalid input",
+            email: `user doesn't exist`
+          });
+        } else {
+          return res.status(500).json({
+            error: true,
+            message: e,
+          });
+        }
+      }
+
+    });
+
+    form.on('fileBegin', function (name, file) {
+      file.filepath = 'public/uploads/images/users/' + file.originalFilename;
+    });
+
+  } catch (e) {
+    return res.status(500).json({
+      error: true,
+      message: e,
+    });
+  }
+
+});
+
+/**
+ * Route for deleting a user
+ * Feneel Doshi
+ */
+ usersRouter.route("/delete").delete(async (req, res) => {
+
+  if(!req.session || !req.session.email){
+    return res.status(401).json({
+      error: true,
+      message: "authentication is required",
+    });
+  }
+  const emailId=req.session.email;  
+  try {
+    const deleteUser = await usersData.remove(emailId);
+    res.json(deleteUser);
+  } catch (e) {
+    if (e == `No user with email=${emailId}`) {
+      return res.status(404).json({
+        error: true,
+        message: e,
+      });
+    } else {
+      return res.status(500).json({
+        error: true,
+        message: "Something went wrong, please try after sometime",
+      });
+    }
+  }
+});
+
 module.exports = usersRouter;
