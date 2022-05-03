a9de174daaf4e389c16b70b26895ce25c5aed306
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2022-05-03 12:41:24 -0400

    Renamed petTypeList

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const constructorMethod = (app) => {
     app.use('/pets', petsRouter);
     app.use('/users', usersRouter);
     app.use('/', async (req, res) => {
-        let petList = await petTypesData.getAllPetTypes();
-        res.render('home', { title: 'Home', petList: petList });
+        let petTypeList = await petTypesData.getAllPetTypes();
+        res.render('home', { title: 'Home', petTypeList: petTypeList });
     });
     app.use('*', (req, res) => {
         res.status(404).json({ error: 'Not found' });
