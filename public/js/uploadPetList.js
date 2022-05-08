f79ad5879dfe7ca25e494fed66043cdf54bc79be
Dominick Varano <46766544+PaperPony@users.noreply.github.com>
2022-05-08 16:24:42 -0400

    Added some wonderful css classes for buttons and footers. Also added footer.

--- public/js/uploadPetList.js
+++ public/js/uploadPetList.js
@@ -57,7 +57,7 @@
                     '<div><img src=' + imagePath + searchedDataList[count].picture + ' width="250" height="200" alt="' + searchedDataList[count].name + '"></div>' +
                     '<div style="text-align:center;">' + searchedDataList[count].name +
                     '</div>' +
-                    '<div style="text-align:center;">  <button name="details" value=' + searchedDataList[count]._id + ' style="margin-bottom: 20px;">View Details</button></div>' +
+                    '<div style="text-align:center;">  <button class="petListButton" name="details" value=' + searchedDataList[count]._id + ' style="margin-bottom: 20px;">View Details</button></div>' +
                     '</div>');
             }
         }
